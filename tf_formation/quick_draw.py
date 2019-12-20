4fc8bde8393af4bcd2c6af0240efc7cd01dcfb4c
jean delest <jeanlemirates@gmail.com>
2019-12-20 14:33:23 +0100

    Quick draw errors should be checked

--- tf_formation/quick_draw.py
+++ tf_formation/quick_draw.py
@@ -4,32 +4,28 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import NullFormatter
 import os
 
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-data_dir = "quick_draw_dataset"
-files = os.listdir(data_dir)
+data_dir = "C:/Users/djddja/Desktop/quick_draw_dataset"
+files = [name for name in os.listdir(data_dir) if ".npy" in name]
 max_size_per_cl = 1500
 
 draw_class = []
 
 # Evalueate the size of the dataset
 size = 0
-i = 0
 for name in files:
     draws = np.load(os.path.join(data_dir, name))
     # print("file shape: ", draws.shape)
     draws = draws[:max_size_per_cl] # Take only max_size_per_cl draw
     size += draws.shape[0]
-    i += 1
-# print("Images took  %s" % (i * max_size_per_cl))
-# print("sise %s" % size)
+
 images = np.zeros((size, 28, 28))
 targets = np.zeros((size,))
 
 it = 0
 t = 0
-raw_im = []
 for name in files:
     # Open each dataset and add the new class
     draw_class.append(name.replace("full_numpy_bitmap_", "").replace(".npy", ""))
@@ -41,11 +37,21 @@ for name in files:
     # Iter
     it += draws.shape[0]
     t += 1
-    raw_im.append(draws)
-print(raw_im[0][0])
-# plt.imshow(raw_im[0][0], cmap='binary')
-# plt.show()
+
 """
+w=10
+h=10
+fig=plt.figure(figsize=(8, 8))
+columns = 4
+rows = 5
+for i in range(1, columns*rows +1):
+    index = np.random.randint(len(images))
+    img = images[index]
+    fig.add_subplot(rows, columns, i)
+    plt.title(draw_class[int(targets[index])]) 
+    plt.imshow(img, cmap="gray")
+plt.show()"""
+
 images = images.astype(np.float32)
     
 # Shuffle dataset
@@ -54,12 +60,117 @@ np.random.shuffle(indexes)
 images = images[indexes]
 targets = targets[indexes]
 
-im_train, im_test, tar_train, tar_test = train_test_split(images, targets, test_size=0.2, random_state=1)
+im_train, im_validation, tar_train, tar_validation = train_test_split(images, targets, test_size=0.2)
 
-print("images.shape", im_train.shape)
+"""print("images.shape", im_train.shape)
 print("targets.shape", tar_train.shape)
 
 print("images_valid.shape", im_test.shape)
 print("targets_valid.shape", tar_test.shape)
 
 print(draw_class)"""
+
+# Normalisation
+scaler = StandardScaler()
+
+im_train = scaler.fit_transform(im_train.reshape(-1, 28*28))
+im_train = im_train.reshape(-1, 28, 28, 1)
+im_validation = scaler.transform(im_validation.reshape(-1, 28*28))
+im_validation = im_validation.reshape(-1, 28, 28, 1)
+
+# Tf Data Handling
+
+data_im_train = tf.data.Dataset.from_tensor_slices((im_train, tar_train))
+data_im_validation = tf.data.Dataset.from_tensor_slices((im_validation, tar_validation))
+
+"""epoch = 1
+batch_size = 32
+for images_batch, targets_batch in data_im_train.repeat(epoch).batch(32):
+    print(images_batch.shape, targets_batch.shape)
+    break"""
+
+
+class ConvModel(tf.keras.Model):
+    
+    def __init__(self):
+        super(ConvModel, self).__init__()
+        # Convolutions
+        self.conv1 = tf.keras.layers.Conv2D(32, 4, activation='relu', name="conv1")
+        self.conv2 = tf.keras.layers.Conv2D(64, 3, activation='relu', name="conv2")
+        self.conv3 = tf.keras.layers.Conv2D(128, 3, activation='relu', name="conv3")
+        # Flatten the convolution
+        self.flatten = tf.keras.layers.Flatten(name="flatten")       
+        # Dense layers
+        self.d1 = tf.keras.layers.Dense(128, activation='relu', name="d1")
+        self.out = tf.keras.layers.Dense(10, activation='softmax', name="output")
+
+    def call(self, image):
+        conv1 = self.conv1(image)
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+        flatten = self.flatten(conv3)
+        d1 = self.d1(flatten)
+        output = self.out(d1)
+        return output
+
+model = ConvModel()
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam()
+
+train_loss = tf.keras.metrics.MeanAbsoluteError(name='train_loss')
+valid_loss = tf.keras.metrics.MeanAbsoluteError(name='valid_loss')
+# Accuracy
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+valid_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='valid_accuracy')
+
+@tf.function
+def train_step(image, targets):
+    with tf.GradientTape() as tape:
+        # Make a prediction on all the batch
+        predictions = model(image)
+        # Get the error/loss on these predictions
+        loss = loss_object(targets, predictions)
+    # Compute the gradient which respect to the loss
+    gradients = tape.gradient(loss, model.trainable_variables)
+    # Change the weights of the model
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    # The metrics are accumulate over time. You don't need to average it yourself.
+    train_loss(loss)
+    train_accuracy(targets, predictions)
+
+@tf.function
+def valid_step(image, targets):
+    predictions = model(image)
+    t_loss = loss_object(targets, predictions)
+    # Set the metrics for the test
+    valid_loss(t_loss)
+    valid_accuracy(targets, predictions)
+
+epoch = 10
+batch_size = 32
+b = 0
+for epoch in range(epoch):
+    # Training set
+    for images_batch, targets_batch in data_im_train.batch(batch_size):
+        train_step(images_batch, targets_batch)
+        template = '\r Batch {}/{}, Loss: {}, Accuracy: {}'
+        print(template.format(
+            b, len(targets), train_loss.result(), 
+            train_accuracy.result()*100
+        ), end="")
+        b += batch_size
+    # Validation set
+    for images_batch, targets_batch in data_im_validation.batch(batch_size):
+        valid_step(images_batch, targets_batch)
+
+    template = '\nEpoch {}, Valid Loss: {}, Valid Accuracy: {}'
+    print(template.format(
+        epoch+1,
+        valid_loss.result(), 
+        valid_accuracy.result()*100)
+    )
+    valid_loss.reset_states()
+    valid_accuracy.reset_states()
+    train_accuracy.reset_states()
+    train_loss.reset_states()
